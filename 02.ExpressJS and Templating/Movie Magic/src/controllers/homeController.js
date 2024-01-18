4cb5858fb4c119887129e6b552998a3bc1f75e92
Todor Mladenov <toshko958@gmail.com>
2024-01-18 20:49:22 +0100

    Add search page functionality

--- 02.ExpressJS and Templating/Movie Magic/src/controllers/homeController.js
+++ 02.ExpressJS and Templating/Movie Magic/src/controllers/homeController.js
@@ -10,6 +10,13 @@ router.get('/about', (req, res) => {
     res.render('about')
 });
 
+router.get('/search', (req, res) => {
+    const { title, genre, year } = req.query;
+    const movies = movieServices.search(title, genre, year);
+
+    res.render('search', { movies, title, genre, year });
+});
+
 router.get('/404', (req, res) => {
     res.render('404');
 });
--- 02.ExpressJS and Templating/Movie Magic/src/services/movieServices.js
+++ 02.ExpressJS and Templating/Movie Magic/src/services/movieServices.js
@@ -12,3 +12,21 @@ exports.getAll = () => {
 exports.getById = (id) => {
     return movies.find(m => m.id == id);
 }
+
+exports.search = (title, genre, year) => {
+    let result = movies.slice();
+
+    if (title) {
+        result = result.filter(m => m.title.toLocaleLowerCase().includes(title.toLocaleLowerCase()));
+    }
+
+    if (genre) {
+        result = result.filter(m => m.genre.toLocaleLowerCase() === genre.toLocaleLowerCase());
+    }
+
+    if (year) {
+        result = result.filter(m => m.year == year);
+    }
+
+    return result;  
+}
