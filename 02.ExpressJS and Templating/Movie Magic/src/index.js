79934a9493da80cd46257519857411de03ba5c2a
Todor Mladenov <toshko958@gmail.com>
2024-01-18 12:40:57 +0100

    Configure express-handlebars

--- 02.ExpressJS and Templating/Movie Magic/src/index.js
+++ 02.ExpressJS and Templating/Movie Magic/src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
+app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+    
 app.get('/', (req, res) => {
-    res.send('Home Page')
+    res.render('home');
 })
 
 app.listen(port, () => console.log('Server is listening on port 3000...'));
