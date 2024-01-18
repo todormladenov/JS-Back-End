21876156fe0e88da4bf30e3aa9d49b2bb8b8bfda
Todor Mladenov <toshko958@gmail.com>
2024-01-18 14:15:11 +0100

    Extract routes
    Add homes controller

--- 02.ExpressJS and Templating/Movie Magic/src/index.js
+++ 02.ExpressJS and Templating/Movie Magic/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
+const router = require('./routes');
 
 const app = express();
 const port = 3000;
@@ -10,9 +11,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-    
-app.get('/', (req, res) => {
-    res.render('home');
-})
+
+app.use(router);
 
 app.listen(port, () => console.log('Server is listening on port 3000...'));
