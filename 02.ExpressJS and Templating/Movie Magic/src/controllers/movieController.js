ded769022951810d884ce100ef0c47433619fb8f
Todor Mladenov <toshko958@gmail.com>
2024-01-18 18:53:48 +0100

    Add create functionality

--- 02.ExpressJS and Templating/Movie Magic/src/controllers/movieController.js
+++ 02.ExpressJS and Templating/Movie Magic/src/controllers/movieController.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const movieServices = require('../services/movieServices');
 
 router.get('/movie/create', (req, res) => {
     res.render('movieCreate');
+});
+
+router.post('/movie/create', (req, res) => {
+    const newMovie = req.body;
+    movieServices.create(newMovie);
+
+    res.redirect('/');
 });
 
 module.exports = router;
--- 02.ExpressJS and Templating/Movie Magic/src/services/movieServices.js
+++ 02.ExpressJS and Templating/Movie Magic/src/services/movieServices.js
@@ -1,5 +1,10 @@
 const movies = require('../data/movies.json');
 
+exports.create = (movieData) => {
+    movieData.id = movies[movies.length - 1].id + 1;
+    movies.push(movieData);
+}
+
 exports.getAll = () => {
     return movies.slice();
 }
