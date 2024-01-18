578e9f04b954188cd408b4fc8fe892849399bed2
Todor Mladenov <toshko958@gmail.com>
2024-01-18 12:52:20 +0100

    Set up static resources

--- 02.ExpressJS and Templating/Movie Magic/src/index.js
+++ 02.ExpressJS and Templating/Movie Magic/src/index.js
@@ -8,6 +8,8 @@ const port = 3000;
 app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
     
 app.get('/', (req, res) => {
     res.render('home');
