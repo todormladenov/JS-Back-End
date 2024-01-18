913604636c4cce832f71ae84b1f5a7c7ab8ceb03
Todor Mladenov <toshko958@gmail.com>
2024-01-18 12:04:46 +0100

    Set up express

--- 02.ExpressJS and Templating/Movie Magic/src/index.js
+++ 02.ExpressJS and Templating/Movie Magic/src/index.js
@@ -1,1 +1,10 @@
-console.log('Working');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Home Page')
+})
+
+app.listen(port, () => console.log('Server is listening on port 3000...'));
