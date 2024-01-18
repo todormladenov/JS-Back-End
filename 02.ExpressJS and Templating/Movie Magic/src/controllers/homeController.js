bbe77f67464a77e062acaf644b99f6de174fed71
Todor Mladenov <toshko958@gmail.com>
2024-01-18 18:47:39 +0100

    Add dynamism to home page

--- 02.ExpressJS and Templating/Movie Magic/src/controllers/homeController.js
+++ 02.ExpressJS and Templating/Movie Magic/src/controllers/homeController.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const movieServices = require('../services/movieServices');
 
 router.get('/', (req, res) => {
-    res.render('home');
+    const movies = movieServices.getAll();
+    res.render('home', { movies });
 });
 
 router.get('/about', (req, res) => {
