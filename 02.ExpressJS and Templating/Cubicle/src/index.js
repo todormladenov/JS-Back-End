94fc06650d6bb950350db5f74c723da114ac8764
Todor Mladenov <toshko958@gmail.com>
2024-01-19 12:12:15 +0100

    Add static resources

--- 02.ExpressJS and Templating/Cubicle/src/index.js
+++ 02.ExpressJS and Templating/Cubicle/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const configHandlebars = require('./config/configHandlebars');
+const configExpress = require('./config/configExpress');
 
 const app = express();
 const port = 3000;
 
 configHandlebars(app);
+configExpress(app);
 
 app.get('/', (req, res) => {
     res.render('home')
