e2dc8a5ce6af10eb14a6fc8ba763b1970f0ce361
Todor Mladenov <toshko958@gmail.com>
2024-01-19 12:00:01 +0100

    Set up handlebars

---
+++ 02.ExpressJS and Templating/Cubicle/src/config/configHandlebars.js
@@ -0,0 +1,10 @@
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+function configHandlebars(app) {
+    app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+    app.set('view engine', 'hbs');
+    app.set('views', path.join(__dirname, '..', 'views'));
+}
+
+module.exports = configHandlebars;
--- 02.ExpressJS and Templating/Cubicle/src/index.js
+++ 02.ExpressJS and Templating/Cubicle/src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const configHandlebars = require('./config/configHandlebars');
 
 const app = express();
 const port = 3000;
 
+configHandlebars(app);
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('home')
 });
 
 app.listen(port, console.log(`Server is listening on port ${port}...`));
