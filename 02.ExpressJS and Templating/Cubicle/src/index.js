36e409f96327df7abac28985456b8b5b4e7237f9
Todor Mladenov <toshko958@gmail.com>
2024-01-19 12:49:28 +0100

    Create router and home controller

--- 02.ExpressJS and Templating/Cubicle/src/index.js
+++ 02.ExpressJS and Templating/Cubicle/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const configHandlebars = require('./config/configHandlebars');
 const configExpress = require('./config/configExpress');
+const router = require('./routes');
 
 const app = express();
 const port = 3000;
@@ -8,8 +9,6 @@ const port = 3000;
 configHandlebars(app);
 configExpress(app);
 
-app.get('/', (req, res) => {
-    res.render('home')
-});
+app.use(router);
 
 app.listen(port, console.log(`Server is listening on port ${port}...`));
