246c59824df041fba154e1581e648f439a4a80f9
Todor Mladenov <toshko958@gmail.com>
2024-01-25 18:36:45 +0100

    Add mongoose

--- 02.ExpressJS and Templating/Movie Magic/src/index.js
+++ 02.ExpressJS and Templating/Movie Magic/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = require('./routes');
 const configHandlebars = require('./config/configHandlebars');
 const configExpress = require('./config/configExpress');
@@ -9,6 +10,10 @@ const port = 3000;
 configHandlebars(app);
 configExpress(app);
 
+
 app.use(router);
+
+mongoose.connect('mongodb://127.0.0.1:27017/magic-movies')
+    .then(() => console.log('DB is Connected'));
 
 app.listen(port, () => console.log('Server is listening on port 3000...'));
