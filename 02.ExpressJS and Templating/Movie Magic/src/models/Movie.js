7490e8263fc6889ba54909c0e8f6f88c4133dff2
Todor Mladenov <toshko958@gmail.com>
2024-01-25 19:13:42 +0100

    Create Movie model

---
+++ 02.ExpressJS and Templating/Movie Magic/src/models/Movie.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    director: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+        max: 2030,
+        min: 1900,
+    },
+    rating: {
+        type: Number,
+        required: true,
+        max: 5,
+        min: 1,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 1000,
+    },
+    imageURL: {
+        type: String,
+        required: true,
+        match: /^https?:\/\//
+    }
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
