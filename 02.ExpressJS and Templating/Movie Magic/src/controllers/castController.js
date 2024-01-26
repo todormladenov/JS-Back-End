d9ce2bf967499590e84af16b46e2e41f95a5132a
Todor Mladenov <toshko958@gmail.com>
2024-01-26 12:22:07 +0100

    Create cast service and create cast functionality

--- 02.ExpressJS and Templating/Movie Magic/src/controllers/castController.js
+++ 02.ExpressJS and Templating/Movie Magic/src/controllers/castController.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
+const castServices = require('../services/castServices');
 
 router.get('/cast/create', (req, res) => {
     res.render('cast/create');
 });
+
+router.post('/cast/create', async (req, res) => {
+    const castData = req.body;
+
+    await castServices.create(castData);
+
+    res.redirect('/cast/create');
+});
+
 
 module.exports = router;
