ff5cc0a68bf73a32ccdea9e4f603ab4a98148095
Todor Mladenov <toshko958@gmail.com>
2024-01-27 17:43:23 +0100

    Add Cube model

---
+++ 02.ExpressJS and Templating/Cubicle/src/models/Cube.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const cubeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 500
+    },
+    imgUrl: {
+        type: String,
+        required: true,
+        match: /^https?:\/\//,
+    },
+    difficulty: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 6
+    },
+    accessories: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'Accessory'
+    }]
+});
+
+const Cube = mongoose.model('Cube', cubeSchema);
+
+module.exports = Cube;
