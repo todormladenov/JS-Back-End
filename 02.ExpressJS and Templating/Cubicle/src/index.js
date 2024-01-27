49842e0f21e7ee721f70a539d4b6daa2f9638a0b
Todor Mladenov <toshko958@gmail.com>
2024-01-27 17:43:08 +0100

    Set up mongoose

--- 02.ExpressJS and Templating/Cubicle/src/index.js
+++ 02.ExpressJS and Templating/Cubicle/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const configHandlebars = require('./config/configHandlebars');
 const configExpress = require('./config/configExpress');
 const router = require('./routes');
@@ -10,5 +11,8 @@ configHandlebars(app);
 configExpress(app);
 
 app.use(router);
+
+mongoose.connect('mongodb://127.0.0.1:27017/cubes')
+    .then(() => console.log('DB Connected'));
 
 app.listen(port, console.log(`Server is listening on port ${port}...`));
