a1126f67cbb4ce1aa06ca5737ed654b1ac1cd814
Todor Mladenov <toshko958@gmail.com>
2024-01-28 10:30:45 +0100

    Add create accessory functionality

--- 02.ExpressJS and Templating/Cubicle/src/controllers/accessoryController.js
+++ 02.ExpressJS and Templating/Cubicle/src/controllers/accessoryController.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
+const accessoryServices = require('../services/accessoryServices');
 
 router.get('/accessory/create', (req, res) => {
     res.render('accessory/create')
+});
+
+router.post('/accessory/create', async (req, res) => {
+    const newAccessory = req.body;
+
+    await accessoryServices.create(newAccessory);
+
+    res.redirect('/')
 });
 
 module.exports = router;
