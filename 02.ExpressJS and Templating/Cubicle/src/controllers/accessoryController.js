13fd163dc2a52fa4a83cc77bd46894238273ae7b
Todor Mladenov <toshko958@gmail.com>
2024-01-28 15:41:58 +0100

    Add dynamic accessory view

--- 02.ExpressJS and Templating/Cubicle/src/controllers/accessoryController.js
+++ 02.ExpressJS and Templating/Cubicle/src/controllers/accessoryController.js
@@ -13,4 +13,11 @@ router.post('/accessory/create', async (req, res) => {
     res.redirect('/')
 });
 
+router.get('/accessory', async (req, res) => {
+    const accessories = await accessoryServices.getAll().lean();
+
+    res.render('accessory/accessory', { accessories });
+});
+
+
 module.exports = router;
--- 02.ExpressJS and Templating/Cubicle/src/services/accessoryServices.js
+++ 02.ExpressJS and Templating/Cubicle/src/services/accessoryServices.js
@@ -5,3 +5,7 @@ exports.create = (newAccessory) => Accessory.create(newAccessory);
 exports.getAll = () => Accessory.find();
 
 exports.attach = (accId, cubeId) => Accessory.findByIdAndUpdate(accId, { $push: { cubes: cubeId } });
+
+exports.getById = (id) => Accessory.findById(id).populate('cubes');
+
+exports.search = (name) => Accessory.find({ name: new RegExp(name, 'i') }); 
