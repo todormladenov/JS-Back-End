b34c78b81f32ecbede70ef804d4d082d079d7359
Todor Mladenov <toshko958@gmail.com>
2024-02-01 12:34:14 +0100

    Add user controller and register view

--- 02.ExpressJS and Templating/Movie Magic/src/routes.js
+++ 02.ExpressJS and Templating/Movie Magic/src/routes.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const movieController = require('./controllers/movieController');
 const castController = require('./controllers/castController');
+const userController = require('./controllers/userController');
 
 router.use(homeController);
 router.use(movieController);
 router.use(castController);
+router.use(userController);
 
 router.get('*', (req, res) => {
     res.redirect('/404');
