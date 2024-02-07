51da3530b9205956231fc03e08afb740593ce83e
Todor Mladenov <toshko958@gmail.com>
2024-02-07 11:41:27 +0100

    Add home controller

--- Exam Prep/01.Second Hand Electronics/src/index.js
+++ Exam Prep/01.Second Hand Electronics/src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const configExpress = require('./config/configExpress');
 const configHandlebars = require('./config/configHandlebars');
+const router = require('./router');
 
 const app = express();
+const port = 3000;
 
 configExpress(app);
 configHandlebars(app);
 
-app.get('/', (req, res) => {
-    res.render('home')
-});
+app.use(router)
 
-app.listen(3000, () => console.log('Server is listening on port 3000'));
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
