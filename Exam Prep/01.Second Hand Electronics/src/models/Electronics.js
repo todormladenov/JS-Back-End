a8276bc82014e2f41f3f4bda7101ceb55c900b6f
Todor Mladenov <toshko958@gmail.com>
2024-02-08 16:09:48 +0100

    Update models

--- Exam Prep/01.Second Hand Electronics/src/models/Electronics.js
+++ Exam Prep/01.Second Hand Electronics/src/models/Electronics.js
@@ -3,35 +3,45 @@ const mongoose = require('mongoose');
 const electronicsSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        minLength: 10
     },
     type: {
         type: String,
-        required: true
+        required: true,
+        minLength: 2
     },
     damages: {
         type: String,
-        required: true
+        required: true,
+        minLength: 10
     },
     image: {
         type: String,
-        required: true
+        required: true,
+        match: [/^https?:\/\//, 'Movie Poster should start with http:// or https://']
     },
     description: {
         type: String,
-        required: true
+        required: true,
+        minLength: 10,
+        maxLength: 200,
     },
     production: {
-        type: String,
-        required: true
+        type: Number,
+        required: true,
+        min: 1900,
+        max: 2023
     },
     exploitation: {
-        type: String,
-        required: true
+        type: Number,
+        required: true,
+        min: 1
     },
     price: {
         type: Number,
-        required: true
+        required: true,
+        min: 1
     },
     buyingList: [{
         type: mongoose.Types.ObjectId,
--- Exam Prep/01.Second Hand Electronics/src/models/User.js
+++ Exam Prep/01.Second Hand Electronics/src/models/User.js
@@ -4,16 +4,19 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        minLength: 3
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        minLength: 10
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        minLength: 4
     }
 });
 
