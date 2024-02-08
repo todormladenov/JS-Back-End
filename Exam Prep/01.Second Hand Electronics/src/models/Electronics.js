618201d8e8cbce8756c93017ed8af2ce9d658add
Todor Mladenov <toshko958@gmail.com>
2024-02-08 08:59:52 +0100

    Add Electronics model

---
+++ Exam Prep/01.Second Hand Electronics/src/models/Electronics.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const electronicsSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    damages: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    production: {
+        type: String,
+        required: true
+    },
+    exploitation: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    buyingList: [{
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    }],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+    }
+});
+
+const Electronics = mongoose.model('Electronics', electronicsSchema);
+
+module.exports = Electronics;
