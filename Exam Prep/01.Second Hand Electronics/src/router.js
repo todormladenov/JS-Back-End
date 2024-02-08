c8e78c9553d41b491f9fbf0f3677a13c2f438de3
Todor Mladenov <toshko958@gmail.com>
2024-02-08 09:26:31 +0100

    Add 404 page

--- Exam Prep/01.Second Hand Electronics/src/router.js
+++ Exam Prep/01.Second Hand Electronics/src/router.js
@@ -7,4 +7,8 @@ router.use(homeController);
 router.use(userController);
 router.use(electronicsController);
 
+router.get('*', (req, res) => {
+    res.redirect('/404');
+})
+
 module.exports = router;
