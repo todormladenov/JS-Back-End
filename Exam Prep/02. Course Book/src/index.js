77d6005be9dfa1aa044a01782685a401112539f8
Todor Mladenov <toshko958@gmail.com>
2024-02-09 15:39:04 +0100

    Add router and home controller

--- Exam Prep/02. Course Book/src/index.js
+++ Exam Prep/02. Course Book/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { configExpress } = require('./config/configExpress');
 const { configHandlebars } = require('./config/configHandlebars');
+const router = require('./router');
 
 const port = 3000;
 const app = express();
 
 configExpress(app);
 configHandlebars(app);
+
+app.use(router);
 
 app.listen(port, () => console.log('Server is listening'));
