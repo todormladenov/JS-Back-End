1daa7012cdf09c5c2829260c36a5b50b442fd542
Todor Mladenov <toshko958@gmail.com>
2024-02-10 11:27:06 +0100

    Add login functionality

--- Exam Prep/02. Course Book/src/services/userServices.js
+++ Exam Prep/02. Course Book/src/services/userServices.js
@@ -5,7 +5,7 @@ const { SECRET } = require('../config/config');
 
 exports.register = async ({ username, email, password, rePassword }) => {
     const existingUser = await User.findOne({ $or: [{ email: email }, { username: username }] });
-    
+
     if (existingUser) {
         throw new Error('Email already exists');
     }
@@ -22,7 +22,30 @@ exports.register = async ({ username, email, password, rePassword }) => {
     const user = await User.create({ username, email, password: hash });
     const payload = {
         _id: user._id,
-        email: user._id 
+        email: user._id
+    }
+
+    const token = await jwt.sign(payload, SECRET);
+
+    return token;
+};
+
+exports.login = async ({ email, password }) => {
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+        throw new Error('Email or password don\'t match');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Email or password don\'t match');
+    }
+
+    const payload = {
+        _id: user._id,
+        email: user._id
     }
 
     const token = await jwt.sign(payload, SECRET);
