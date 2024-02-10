5bf3f62e69f4d1563aefcc8bdc18ee168a67e995
Todor Mladenov <toshko958@gmail.com>
2024-02-10 12:33:13 +0100

    Updated home view

--- Exam Prep/02. Course Book/src/controllers/homeController.js
+++ Exam Prep/02. Course Book/src/controllers/homeController.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const courseServices = require('../services/courseServices');
 
-router.get('/', (req, res) => {
-    res.render('home');
+router.get('/', async (req, res) => {
+    const courses = await courseServices.getLastThree().lean();
+    res.render('home', { courses });
 });
 
 module.exports = router;
--- Exam Prep/02. Course Book/src/services/courseServices.js
+++ Exam Prep/02. Course Book/src/services/courseServices.js
@@ -1,3 +1,5 @@
 const Course = require('../models/Course');
 
 exports.create = (courseData) => Course.create(courseData);
+
+exports.getLastThree = () => Course.find().sort({ createdAt: -1 }).limit(3);
