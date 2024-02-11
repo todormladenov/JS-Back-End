8352aeea420301705f7bf22a6ff1c0ce7836ae1d
Todor Mladenov <toshko958@gmail.com>
2024-02-11 10:47:00 +0100

    Add router and home controller

--- Exam Prep/03.Wizard Creatures Wiki/src/index.js
+++ Exam Prep/03.Wizard Creatures Wiki/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { configExpress } = require('./config/configExpress');
 const { configHandlebars } = require('./config/configHandlebars');
+const router = require('./router');
 
 const app = express();
 const port = 3000;
 
 configExpress(app);
 configHandlebars(app);
+
+app.use(router);
 
 app.listen(port, () => console.log('Server is listening'));
