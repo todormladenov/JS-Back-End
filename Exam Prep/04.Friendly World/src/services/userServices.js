9777b5f6f931421b5bf95b1bfa535077f37aa6a1
Todor Mladenov <toshko958@gmail.com>
2024-02-14 22:29:28 +0100

    Add login and logout functionality

--- Exam Prep/04.Friendly World/src/services/userServices.js
+++ Exam Prep/04.Friendly World/src/services/userServices.js
@@ -25,3 +25,21 @@ exports.register = async ({ email, password, rePassword }) => {
 
     return token;
 };
+
+exports.login = async ({ email, password }) => {
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+        throw new Error('Email or password don\'t match');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Email or password don\'t match');
+    }
+
+    const token = await jwt.sign({ _id: user._id, email: user.email }, SECRET);
+
+    return token;
+}
