a1ba5992a77a64c498eece5c555012378478f033
Todor Mladenov <toshko958@gmail.com>
2024-02-14 22:15:27 +0100

    Add auth middleware

--- Exam Prep/04.Friendly World/src/config/configExpress.js
+++ Exam Prep/04.Friendly World/src/config/configExpress.js
@@ -1,9 +1,11 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const path = require('path');
+const { auth } = require('../middlewares/authMiddleware');
 
 exports.configExpress = (app) => {
     app.use(express.static(path.join(__dirname, '..', 'public')));
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
+    app.use(auth);
 };
