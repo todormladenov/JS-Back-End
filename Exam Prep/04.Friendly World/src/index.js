32276038573facefab4ae70a4faf087de2723da0
Todor Mladenov <toshko958@gmail.com>
2024-02-14 14:26:40 +0100

    Set up handlebars

--- Exam Prep/04.Friendly World/src/index.js
+++ Exam Prep/04.Friendly World/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { configExpress } = require('./config/configExpress');
+const { configHandlebars } = require('./config/configHandlebars');
 
 const app = express();
 const port = 3000;
 
 configExpress(app);
+configHandlebars(app);
 
 app.listen(port, () => console.log('Server is listening'));
